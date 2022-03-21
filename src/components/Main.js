ce266821dd99cfebd805efcf51fa73064178e8ef
Jeewon Lee <jeewon.jlee@gmail.com>
2022-03-21 17:28:32 -0400

    Add underline effect to navbar and setup main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,19 @@ import React from "react";
 import "./Main.css";
 
 const Main = () => {
-  return <div className="main-container">Main</div>;
+  return (
+    <div className="main-container">
+      <div className="main-title">
+        <div className="profile-image" />
+        <div className="profile-description">
+          <h1>
+            Hi there, my name is <span id="first-name">Jeewon</span>
+          </h1>
+          <h2>and I'm a full stack developer</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;

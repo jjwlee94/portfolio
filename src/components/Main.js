f5b4b0763de6cb8666c0114bc04597fc21767219
Jeewon Lee <jeewon.jlee@gmail.com>
2022-03-22 23:37:43 -0400

    Complete responsive design

--- src/components/Main.js
+++ src/components/Main.js
@@ -61,9 +61,17 @@ const Main = () => {
             </h2>
             <h2 id="profile-sns">
               Find me here
-              <AiFillGithub fontSize={20} id="sns-icons" />
-              <AiFillLinkedin fontSize={20} id="sns-icons" />
-              <AiFillMail fontSize={20} id="sns-icons" />
+              <a href="https://www.github.com/jjwlee94" id="sns-link">
+                <AiFillGithub fontSize={20} id="sns-icons" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/jeewon-lee-dev"
+                id="sns-link">
+                <AiFillLinkedin fontSize={20} id="sns-icons" />
+              </a>
+              <a href="mailto:jeewon.jlee@gmail.com" id="sns-link">
+                <AiFillMail fontSize={20} id="sns-icons" />
+              </a>
             </h2>
           </div>
         </div>
@@ -81,104 +89,104 @@ const Main = () => {
             My <span id="stack-title">Stack</span>
           </h1>
         </div>
-
-        <div className="stack-cards">
-          <div id="stack-icon">
-            <SiJavascript fontSize={40} />
-            <span id="stack-name">JavaScript</span>
+        <div className="stack-section">
+          <div className="stack-one">
+            <div className="stack-cards">
+              <div id="stack-icon">
+                <SiJavascript fontSize={40} />
+                <span id="stack-name">JavaScript</span>
+              </div>
+              <div id="stack-icon">
+                <SiHtml5 fontSize={40} />
+                <span id="stack-name">HTML</span>
+              </div>
+              <div id="stack-icon">
+                <SiCss3 fontSize={40} />
+                <span id="stack-name">CSS</span>
+              </div>
+            </div>
+            <div className="stack-cards">
+              <div id="stack-icon">
+                <SiRuby fontSize={40} />
+                <span id="stack-name">Ruby</span>
+              </div>
+              <div id="stack-icon">
+                <SiGit fontSize={40} />
+                <span id="stack-name">Git</span>
+              </div>
+              <div id="stack-icon">
+                <SiReact fontSize={40} />
+                <span id="stack-name">React</span>
+              </div>
+            </div>
+            <div className="stack-cards">
+              <div id="stack-icon">
+                <SiNodedotjs fontSize={40} />
+                <span id="stack-name">Node</span>
+              </div>
+              <div id="stack-icon">
+                <SiExpress fontSize={40} />
+                <span id="stack-name">Express</span>
+              </div>
+              <div id="stack-icon">
+                <SiRubyonrails fontSize={40} />
+                <span id="stack-name">Rails</span>
+              </div>
+            </div>
+            <div className="stack-cards">
+              <div id="stack-icon">
+                <SiBootstrap fontSize={40} />
+                <span id="stack-name">Bootstrap</span>
+              </div>
+              <div id="stack-icon">
+                <SiMaterialui fontSize={40} />
+                <span id="stack-name">Material UI</span>
+              </div>
+            </div>
           </div>
-          <div id="stack-icon">
-            <SiHtml5 fontSize={40} />
-            <span id="stack-name">HTML</span>
-          </div>
-          <div id="stack-icon">
-            <SiCss3 fontSize={40} />
-            <span id="stack-name">CSS</span>
-          </div>
-        </div>
-
-        <div className="stack-cards">
-          <div id="stack-icon">
-            <SiRuby fontSize={40} />
-            <span id="stack-name">Ruby</span>
-          </div>
-          <div id="stack-icon">
-            <SiGit fontSize={40} />
-            <span id="stack-name">Git</span>
-          </div>
-        </div>
-
-        <div className="stack-cards">
-          <div id="stack-icon">
-            <SiReact fontSize={40} />
-            <span id="stack-name">React</span>
-          </div>
-          <div id="stack-icon">
-            <SiNodedotjs fontSize={40} />
-            <span id="stack-name">Node</span>
-          </div>
-          <div id="stack-icon">
-            <SiExpress fontSize={40} />
-            <span id="stack-name">Express</span>
-          </div>
-        </div>
-
-        <div className="stack-cards">
-          <div id="stack-icon">
-            <SiRubyonrails fontSize={40} />
-            <span id="stack-name">Rails</span>
-          </div>
-          <div id="stack-icon">
-            <SiBootstrap fontSize={40} />
-            <span id="stack-name">Bootstrap</span>
-          </div>
-          <div id="stack-icon">
-            <SiMaterialui fontSize={40} />
-            <span id="stack-name">Material UI</span>
-          </div>
-        </div>
-
-        <div className="stack-cards">
-          <div id="stack-icon">
-            <SiMocha fontSize={40} />
-            <span id="stack-name">Mocha</span>
-          </div>
-          <div id="stack-icon">
-            <SiChai fontSize={40} />
-            <span id="stack-name">Chai</span>
-          </div>
-          <div id="stack-icon">
-            <SiJest fontSize={40} />
-            <span id="stack-name">Jest</span>
-          </div>
-        </div>
-
-        <div className="stack-cards">
-          <div id="stack-icon">
-            <SiMongodb fontSize={40} />
-            <span id="stack-name">MongoDB</span>
-          </div>
-          <div id="stack-icon">
-            <SiPostgresql fontSize={40} />
-            <span id="stack-name">PostgreSQL</span>
-          </div>
-          <div id="stack-icon">
-            <SiMysql fontSize={40} />
-            <span id="stack-name">MySQL</span>
-          </div>
-        </div>
-        <div className="stack-cards">
-          <div id="stack-icon">
-            <SiDocker fontSize={40} />
-            <span id="stack-name">Docker</span>
-          </div>
-          <div id="stack-icon">
-            <SiKubernetes fontSize={40} />
-            <span id="stack-name">Kubernetes</span>
-          </div>
-          <div id="stack-icon">
-            <SiGooglecloud fontSize={40} />
-            <span id="stack-name">Google Cloud</span>
+          <div className="stack-two">
+            <div className="stack-cards">
+              <div id="stack-icon">
+                <SiMocha fontSize={40} />
+                <span id="stack-name">Mocha</span>
+              </div>
+              <div id="stack-icon">
+                <SiChai fontSize={40} />
+                <span id="stack-name">Chai</span>
+              </div>
+              <div id="stack-icon">
+                <SiJest fontSize={40} />
+                <span id="stack-name">Jest</span>
+              </div>
+            </div>
+            <div className="stack-cards">
+              <div id="stack-icon">
+                <SiMongodb fontSize={40} />
+                <span id="stack-name">MongoDB</span>
+              </div>
+              <div id="stack-icon">
+                <SiPostgresql fontSize={40} />
+                <span id="stack-name">PostgreSQL</span>
+              </div>
+              <div id="stack-icon">
+                <SiMysql fontSize={40} />
+                <span id="stack-name">MySQL</span>
+              </div>
+            </div>
+            <div className="stack-cards">
+              <div id="stack-icon">
+                <SiDocker fontSize={40} />
+                <span id="stack-name">Docker</span>
+              </div>
+              <div id="stack-icon">
+                <SiKubernetes fontSize={40} />
+                <span id="stack-name">Kubernetes</span>
+              </div>
+              <div id="stack-icon">
+                <SiGooglecloud fontSize={40} />
+                <span id="stack-name">Google Cloud</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
